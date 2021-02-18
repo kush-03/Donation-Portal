a4cbbd85cffaf5fef6a5f6d843b22413b2df7e01
kush-03 <43854487+kush-03@users.noreply.github.com>
2021-02-18 13:45:34 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Donation Portal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing">
+
+			<header id="header" class="alt">
+				<h1 ><strong><a href="home.php" >Donation Portal - </a></strong>   Sparks Foundation</h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="home.php">Home</a></li>
+						<li><a href="#one">About</a></li>
+						<li><a href="#two">Contact</a></li>
+						<li><a href="first.php">Logout</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+			<section id="banner">
+				<h2>Welcome To Sparks Donation Portal!</h2>
+				<p>We are Giving things for happiness <br /> Do a good deed, give to those in need.</p>
+			
+				<ul class="actions">
+					<form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_GcM33Bzilv0Qhs" async> </script> </form>
+				</ul>
+			</section>
+
+			<!-- One -->
+				<section id="one" class="wrapper style1">
+					<div class="container 75%">
+						<div class="row 200%">
+							<div class="6u 12u$(medium)">
+								<header class="major">
+									<h2>Sparks Donation</h2>
+									<p>Every Good Act Counts</p>
+								</header>
+							</div>
+							<div class="6u$ 12u$(medium)">
+								<p style="font-weight: bold; font-style: italic;">We are working with an Objective to Help Others in an hour of need as the true value of one's life can be measured if the person is able to make a significant difference in someone's Life.</p>
+								<p style="font-weight: bold; font-style: italic;">Please Note : This is Just a Demo Portal build as an Internship task By Sparks Foundation.The money you donate here will be directly in my pocket so think twice. </p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- two -->
+				<section id="two" class="wrapper style3 special">
+					<div class="container">
+						<header class="major">
+							<h2>Sparks Foundation Internship</h2>
+							<p>Kush Agarwal</p>
+						</header>
+						<ul class="actions">
+							<li><a href="https://www.linkedin.com/in/kush-agarwal-03/" class="button special big">Get in touch</a></li>
+						</ul>
+					</div>
+				</section>
+
+		<!-- Logos -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="https://github.com/kush-03" class="icon fa-github"></a></li>
+						<li><a href="https://twitter.com/KushAgarwal03" class="icon fa-twitter"></a></li>
+						<li><a href="https://www.linkedin.com/in/kush-agarwal-03/" class="icon fa-linkedin"></a></li>
+					</ul>
+					<ul class="copyright">
+						<li style="font-weight: bold;">&copy; No Copyright Issue, Just Ping Me Anytime @Kush Agarwal</li>
+					</ul>
+				</div>
+			</footer>
+
+	</body>
+</html>
---
+++ validation.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$host="localhost";
+$uname="root";
+$pas="";
+$db_name="userregisteration";
+$tbl_name="usertable";
+
+$link = mysqli_connect("$host","$uname","$pas") or die ("cannot connect");
+mysqli_select_db($link,'userregisteration');
+	
+//session_start();
+
+
+//$con=mysqli_connect('localhost','root','@Kush1234')
+
+//mysqli_select_db($con,'userregisteration');
+if(isset($_POST['submit'])){
+	$email =$_POST['user'];
+	$pass=$_POST['password'];
+
+	$s ="select * from usertable where email= '$email' && password='$pass'";
+
+	$result= mysqli_query($link,$s);	
+
+	$num= mysqli_num_rows($result);
+
+	if($num==1){
+		header('location:home.php');
+		echo "You are Succesfully Logged in";
+	}
+	else{
+		header('location:first.php');
+		
+	}
+	
+}
+
+?>
